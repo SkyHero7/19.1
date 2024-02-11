3032d6cc7ff203de080d9c540729a805a836b548
Roman Makhnev <romanmahnef777@gmail.com>
2024-02-11 14:55:31 +0400

    Fix task

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ def submit():
     if request.method == 'POST':
         name = request.form['name']
         email = request.form['email']
-        # Здесь можно обработать данные из формы, например, сохранить их в базу данных
         return render_template('submit.html', name=name, email=email)
 
 if __name__ == '__main__':
